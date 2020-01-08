3b772703501af9220ef93ee981b8f0b6a4a22d8d
40823107 <40823107@gm.nfu.edu.tw>
2020-01-09 01:13:49 +0800

    add graphice files

--- downloads/dart_ex1/重量轉換dart/重量轉換.dart
+++ downloads/dart_ex1/重量轉換dart/重量轉換.dart
@@ -1,23 +1,28 @@
-GtoB(num g){
-return g*2.2046;
+lbtokg(num lb){
+return lb*0.4536;
 }
-BtoG(num b){
-return b*0.4536;
+
+kgtolb(num kg){
+return kg*2.2046;
 }
-main(){
-var type;
-int len;
-var number;
-List weight= ["25kg", "30lb", "56lb", "14kg", "68lb", "198kg"];
-for (var i in weight) {
-len = i.length;
-type = i[len-2];
-number = i.substring(0, len-2);
-number = int.parse(number);
-if (type == "k"){
-print(" $number kg =  ${GtoB(number).toStringAsFixed(2)} lb");
-}else{
-print(" $number lb =  ${BtoG(number).toStringAsFixed(2)} kg");
-}
-}
-}
+
+main() { 
+  int len; 
+  var type;  
+  var number;
+  List weight = ["25kg", "30lb", "56lb", "14kg", "68lb", "198kg"];
+  for (var data in weight) {
+    
+    len = data.length;    
+    type = data[len-2];   
+    number = data.substring(0, len-2);   
+    number = int.parse(number);
+   
+    if (type == "l"){     
+      print("$number 英磅 =  ${lbtokg(number).toStringAsFixed(4)} 公斤");
+      
+    }else{     
+      print("$number 公斤 =  ${kgtolb(number).toStringAsFixed(4)} 英磅");
+    }
+  }
+  } 
